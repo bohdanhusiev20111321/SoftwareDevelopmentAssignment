e662a0ebd840365c54a2cbbe4ee3233dd3301afe
devcodeboh <devcodeboh@gmail.com>
2025-11-13 21:49:28 +0000

    Updated Developer model

--- GameIdeaPlanner/src/main/kotlin/model/Developer.kt
+++ GameIdeaPlanner/src/main/kotlin/model/Developer.kt
@@ -1,6 +1,11 @@
 package ie.setu.model
 
-class Developer
-{
+data class Developer(
 
-}
+    val developerId: Int,
+    var name: String,
+    var jobTitle: String,
+    var salary: Double,
+    var yearsExperience: Int,
+    var isRetired: Boolean = false 
+)

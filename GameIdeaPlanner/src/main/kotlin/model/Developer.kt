318186aa0ed1c678f933800b82db3fc4b327536b
devcodeboh <devcodeboh@gmail.com>
2025-11-13 21:47:02 +0000

    Revert "Added Developer and Idea data models with required fields."
    This reverts commit c137eefda694a231107dd93019cf5fe3bbce2592.

--- GameIdeaPlanner/src/main/kotlin/model/Developer.kt
+++ GameIdeaPlanner/src/main/kotlin/model/Developer.kt
@@ -1,11 +1,6 @@
 package ie.setu.model
 
-data class Developer(
+class Developer
+{
 
-    val developerId: Int,
-    var name: String,
-    var jobTitle: String,
-    var salary: Double,
-    var yearsExperience: Int,
-    var isRetired: Boolean = false 
-)
+}
